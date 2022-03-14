9d275ace4b7410311dbed4ba4de45fd90968a3ee
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-03-14 19:58:04 +0600

    Initial Phase

---
+++ Submissions/JavaScriptNinja/Day29/TweetPostsProject/js/app.js
@@ -0,0 +1,11 @@
+//input must not have 250 words
+//0/250 (More professional and little challenging)
+// on Submit (validation)
+
+
+//2nd feature (1:30pm)
+//generate time during adding tweet
+//date-fns
+
+console.log(format)
+
